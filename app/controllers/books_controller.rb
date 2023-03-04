8755d5baf803df32919d02940df2f69a42ad2e5f
EC2 Default User <ec2-user@ip-172-31-39-5.ap-northeast-1.compute.internal>
2023-03-04 18:49:38 +0000

    各機能テスト、テストフォルダ実行前。HTML・CSSの調整まで完了。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book=Book.new(book_params)
+    if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to books_path
+    else
+     @books=Book.all
+
+     render :index
+    end
   end
 
   def edit
@@ -20,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path
+    else
+      render :edit
+    end
   end
 
   def destroy
