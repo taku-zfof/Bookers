41a4d99b0c9f29db0101f1ae7830b2acec1fe358
EC2 Default User <ec2-user@ip-172-31-39-5.ap-northeast-1.compute.internal>
2023-03-04 17:15:53 +0000

    編集、削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,17 +6,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book=Book.new(book_params)
-    @book.save
+    book=Book.new(book_params)
+    book.save
     redirect_to books_path
   end
 
-
-  def new
-    @book=Book.new
-  end
-
   def edit
+    @book=Book.find(params[:id])
   end
 
   def show
@@ -24,14 +20,20 @@ class BooksController < ApplicationController
   end
 
   def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path
   end
 
   def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body,)
   end
 
 end
